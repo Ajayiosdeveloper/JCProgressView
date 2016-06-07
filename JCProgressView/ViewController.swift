692de6c95704b92259abc3957f2b842f057fc82b
AjayBabu <ajaybabu.s@exilant.com>
2016-06-07 14:49:32 +0530

    Initial Commit

---
+++ JCProgressView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JCProgressView
+//
+//  Created by ajaybabu singineedi on 07/06/16.
+//  Copyright © 2016 Exilant Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
