fc7ef38bd75f14add32264eb03a8c86bdf717a22
AjayBabu <ajaybabu.s@exilant.com>
2016-06-07 23:10:11 +0530

    Added rounded caps to Gradient layer, adjusted start angle to Top.

--- JCProgressView/JCProgressView.swift
+++ JCProgressView/JCProgressView.swift
@@ -8,31 +8,41 @@
 
 import UIKit
 
-let defaultBackGroundColor = UIColor(colorLiteralRed:0, green: 127/255, blue: 231/255, alpha: 0.1)
+let defaultBackGroundColor = UIColor(colorLiteralRed:0, green: 127/255, blue: 231/255, alpha: 0.1) // As discussed in the requirements
 
 
 public class JCProgressView: UIView {
     
-    private var circularLayer = CAShapeLayer()
+    private var circularLayer = CAShapeLayer() // to make animations more easier by using CAShapeLayer property (strokeEnd).
     
-    public var progressColors:[UIColor]?
+    public var progressColors:[UIColor]? // As discussed in requirements
     
-    public var progressColorLocations:[Double]?
+    // ProgressColors must contains atleast two colors.
     
-    public var allowsMultipleColors = true
+    public var progressColorLocations:[Double]? // As discussed in 
     
-    public var progressColor: UIColor?
+    //requirements. ProgressColorLocations must contains atleast two 
+    
+    //colors.
+
+    
+    public var allowsMultipleColors = true // set this to false to avoid multicolor ring
+    
+    public var progressColor: UIColor? // set this to have a single colored progress View. if it is nil then default color will be 
+        //taken
     
     public override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = defaultBackGroundColor
+        self.backgroundColor = UIColor.clearColor()
         drawCircle(frame)
     }
     
-    public init(frame: CGRect,background:UIColor) {
+    public init(frame: CGRect,colors:[UIColor],locations:[Double]) {
        
         super.init(frame: frame)
         self.backgroundColor = UIColor.clearColor()
+        self.progressColors = colors
+        self.progressColorLocations = locations
         drawCircle(frame)
     }
 
@@ -42,14 +52,22 @@ public class JCProgressView: UIView {
         drawCircle(self.bounds)
     }
     
+    /* Drawing Circle using UIBezier Path with top as Start angle and adding to CAShapeLayer */
+    
     private func drawCircle(rect:CGRect){
-       
-        let path = UIBezierPath(ovalInRect:rect)
+        
+        let center = CGPoint(x:rect.size.width/2, y: rect.size.height/2)
+        
+        let path = UIBezierPath(arcCenter:center , radius: rect.width/2, startAngle:CGFloat(-M_PI_2) , endAngle: CGFloat(M_PI_2 + M_PI) , clockwise: true)
+        
         circularLayer.path = path.CGPath
         circularLayer.lineWidth = self.frame.width/4
         circularLayer.fillColor =  defaultBackGroundColor.CGColor
+        circularLayer.lineCap = kCALineCapRound
         circularLayer.strokeColor = UIColor(red: 0.8078, green: 0.2549, blue: 0.2392, alpha: 1.0).CGColor
     }
+    
+    /* adding Annimation to the strokeEnd property of the CAShapeLayer*/
     
     private func animateCircle(){
     
@@ -67,10 +85,15 @@ public class JCProgressView: UIView {
        
     }
     
+    /* Creating Gradient layer with multi colors and specified locations*/
+    
     private func gradientMask() -> CAGradientLayer {
+       
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = self.bounds
         gradientLayer.allowsEdgeAntialiasing = true
+        
+        if self.allowsMultipleColors{
         
         if  let _ = self.progressColors  where self.progressColors?.count > 1{
             gradientLayer.colors = self.getArrayOfCgColor(self.progressColors!)
@@ -83,8 +106,23 @@ public class JCProgressView: UIView {
         }else{
             gradientLayer.locations = addDefaultColorLocationsToCircle()
         }
+        
+        }
+        else{
+            
+            if let _ = progressColor{
+                
+                gradientLayer.colors = [(progressColor?.CGColor)!,(progressColor?.CGColor)!]
+            }else{
+                
+                gradientLayer.colors = [UIColor.grayColor().CGColor,UIColor.grayColor().CGColor]
+            }
+            
+        }
         return gradientLayer
     }
+    
+    // adding default colors to gradient layer if progressColors.count is nil or less than 2
     
     private func addDefaultColorsToCircle()->[AnyObject]{
     
@@ -92,12 +130,15 @@ public class JCProgressView: UIView {
        return colors
     }
     
+     // adding default color locations to gradient layer if ProgressColorLocations.count is nil or less than 2
+    
     private func addDefaultColorLocationsToCircle()->[NSNumber]{
         
          let colorLocations = [0.0,0.3,0.6,0.9]
      
         return colorLocations
     }
+    
     
     private func getArrayOfCgColor(colors:[UIColor])->[CGColor]{
       
@@ -109,24 +150,8 @@ public class JCProgressView: UIView {
        return cgColorsArray
     }
     
-    public func startAnimation(){
+    private func addGradientLayerToViewLayer(){
         
-        if let _ = self.layer.sublayers{
-            
-            for each in self.layer.sublayers!{
-       
-                each.removeFromSuperlayer()
-            }
-            addGradientLayerToView()
-        }else{
-          
-            addGradientLayerToView()
-        }
-        
-        self.animateCircle()
-    }
-    
-    private func addGradientLayerToView(){
         circularLayer.fillColor = defaultBackGroundColor.CGColor
         let gradientMaskLayer = gradientMask()
         gradientMaskLayer.mask = circularLayer
@@ -134,10 +159,34 @@ public class JCProgressView: UIView {
         layer.addSublayer(gradientMaskLayer)
     }
     
+    /* Start and Stop Animation services exposed to API Users */
+    
+    public func startAnimation(){
+        
+        if let _ = self.layer.sublayers{
+            
+            for each in self.layer.sublayers!{
+                
+                each.removeFromSuperlayer()
+            }
+            addGradientLayerToViewLayer()
+        }else{
+            
+            addGradientLayerToViewLayer()
+        }
+        
+           self.animateCircle()
+    }
+
+    
     public func stopAnimation(){
         
      circularLayer.removeAllAnimations()
      circularLayer.fillColor = nil
+    
     }
+    
+ 
+
 
 }
